61d9349fd3f4a899fd548802d3c293b643abea8b
Anora_TR_18\SURENTHAR S <surenthars@anorasolutions.com>
2024-08-22 14:32:47 +0530

    adding multiply feature to try

--- practice/try.c
+++ practice/try.c
@@ -4,9 +4,14 @@ void add(int a, int b) {
     printf("a + b: %d\n", a+b);
 }
 
+void multiply(int a, int b) {
+    printf("a*b: %d\n", a*b);
+}
+
 int main() {
     // changes made by surenthar
     add(2, 4);
+    multiply(3, 6);
     printf("Hello, from main");
     return 0;
 }
