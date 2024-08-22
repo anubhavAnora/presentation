51ef46917e64915e1db16bb7ea133204efc6bcf5
Anora_TR_23\Anubhav <anubhav@anorasolutions.com>
2024-08-22 14:35:20 +0530

    adding subtract feature to try

--- practice/try.c
+++ practice/try.c
@@ -4,9 +4,14 @@ void add(int a, int b) {
     printf("a + b: %d\n", a+b);
 }
 
+void subtract(int a, int b) {
+    printf("a - b: %d\n", a-b);
+}
+
 int main() {
     // changes made by surenthar
     add(2, 4);
+    subtract(5, 3);
     printf("Hello, from main");
     return 0;
 }
